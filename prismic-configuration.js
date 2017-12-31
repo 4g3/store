1cceef26912d7956fbb74be470f74219f7590a55
John Hillegass <hillegassdev@gmail.com>
2017-12-31 14:59:33 -0500

    update prismic config to use oauth as I have had some auth problems via the prismic api

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,8 @@
 module.exports = {
   apiEndpoint: process.env.prismicEndpoint,
   snipcartKey: process.env.snipcartKey,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
   linkResolver: function (doc) {
     if (doc.type == 'category') {
       return '/category/' + doc.uid;
