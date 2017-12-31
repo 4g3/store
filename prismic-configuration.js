32a57849f8b3d8d5c058700aca51cb0da50059ab
John Hillegass <hillegassdev@gmail.com>
2017-12-31 21:03:04 +0000

    remove OAUTH authentication method and just stick with persitent accesstoken

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -2,8 +2,6 @@ module.exports = {
   apiEndpoint: process.env.prismicEndpoint,
   snipcartKey: process.env.snipcartKey,
   accessToken: process.env.accessToken,
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
   linkResolver: function (doc) {
     if (doc.type == 'category') {
       return '/category/' + doc.uid;
